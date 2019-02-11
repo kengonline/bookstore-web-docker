7e40b0670314c39f77ca7481a8b4d39f31ba53dc
kengonline <keang2537@hotmail.com>
2019-02-11 18:04:53 +0700

    Add component BaseCard

---
+++ src/components/card/BaseCard.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Card } from "antd";
+
+const BaseCard = styled(Card)`
+    &.ant-card {
+        margin: 8px 0px;
+    }
+`
+
+export default BaseCard;
