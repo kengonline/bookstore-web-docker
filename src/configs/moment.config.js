755a1ea9ae43c07773dca85a6a6d37e399da99d4
kengonline <keang2537@hotmail.com>
2019-02-12 11:26:09 +0700

    Remove moment locale

--- src/configs/moment.config.js
+++ src/configs/moment.config.js
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 import EnvConfig from 'src/configs/env.config'
-import 'moment/locale/th'
+// import 'moment/locale/th'
 
 moment.tz.setDefault(EnvConfig.TIMEZONE);
 moment.locale(EnvConfig.DEFAULT_LOCALE)
